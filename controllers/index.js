41e1312807cc969aae40f36237e93a13b4959eea
A Bevers <alisabevers@gmail.com>
2023-05-12 22:04:17 -0400

    added to the handlebars file, linked css, and added handlebars to server.js to make sure it runs on localhost

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const users = require('./users');
+const pages = require('./pages');
 
 // localhost:3001/users
+router.use('/', pages);
 router.use('/users', users);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const path = require("path");
 const controllers = require('./controllers');
 const sequelize = require('./config/connection');
 const models = require('./models');
+const exphbs = require('express-handlebars');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
